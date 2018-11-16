a02102505daf0d737ae28fe2334968e6de3c88bd
HAMMEN Maxence <maxence.hammen@gmail.com>
2018-11-17 00:38:29 +0100

    Initial commit

---
+++ core.py
@@ -0,0 +1,67 @@
+import discord
+import re
+import shlex
+import subprocess
+
+import config
+
+
+def is_valid_hostname(hostname):
+    if len(hostname) > 255:
+        return False
+    if hostname[-1] == ".":
+        hostname = hostname[
+                   :-1]  # strip exactly one dot from the right, if present
+    allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(allowed.match(x) for x in hostname.split("."))
+
+
+def is_empty_message(message):
+    return message in ['\n', '\r\n']
+
+
+async def do_ping_rule(message, client):
+    ping_prefix = 'ping '
+    command = message.content
+
+    if not command.startswith(ping_prefix):
+        return False
+
+    remaining_str = command[len(ping_prefix):]
+    if not is_valid_hostname(remaining_str):  # Print an error
+        response = 'the ping request could not find host {}' \
+            .format(shlex.quote(remaining_str))
+        await client.send_message(message.channel, response)
+    else:
+        job = 'ping {}'.format(shlex.quote(remaining_str))
+        process = subprocess.Popen(
+            job, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        )
+        while True:
+            output = process.stdout.readline()
+            if output == '' and process.poll() is not None:
+                break
+            if output:
+                print(output)
+                output_str = output.decode('utf-8')
+                if not is_empty_message(output_str):
+                    await client.send_message(message.channel, output_str)
+        process.poll()
+
+    return True
+
+
+def main():
+    client = discord.Client()
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:  # If user is the bot itself, ignore
+            return
+        await do_ping_rule(message, client)
+
+    client.run(config.TOKEN)
+
+
+if __name__ == '__main__':
+    main()
